7e43f13ea0b9dbb85f117572ea1e1e979a4e7fed
hjn0415 <hjn0415a@gmail.com>
2024-09-27 14:08:09 +0900

    fix: adjust swiper-slide size for mobile screens

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,9 +1,14 @@
+/*
+Configures swipeable cards for features like webapps.
+Requires swiper.bundle.js to be loaded from a CDN first.
+See layouts/_defaults/base_of.html
+*/
 $(document).ready(function(){
     const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
     grabCursor: true,
-    spaceBetween: 55,
+    spaceBetween: 57,
 
     // If we need pagination
     pagination: {
@@ -17,42 +22,33 @@ $(document).ready(function(){
     },
   
     breakpoints: {
-      720: {
-        slidesPerView: 1
+      0: {
+        slidesPerView: 1,
       },
-      820: {
-        slidesPerView: 2
+      700: {
+        slidesPerView: 2,
       },
-      1024: {
-        slidesPerView: 4
+      1000: {
+        slidesPerView: 3,
+      },
+      1350: {
+        slidesPerView: 4,
       }
     }
   });
 
   const swiperSlides = document.querySelectorAll('.swiper-slide');
   
-  /*swiperSlides.forEach(slide => {
-      const paragraphs = slide.querySelectorAll('p'); 
-      slide.addEventListener('mouseover', () => {
-          slide.style.height = '90%';
-          paragraphs.forEach(p => {
-             p.style.display = 'block';
-          });
-      });
-      slide.addEventListener('mouseout', () => {
-          slide.style.height = '65%';
-          paragraphs.forEach(p => {
-             p.style.display = 'none';
-          });
-       
-      });
-  });*/
    swiperSlides.forEach(slide => {
         const paragraphs = slide.querySelectorAll('p');
 
         slide.addEventListener('mouseover', () => {
             slide.style.height = '90%';
         });
+
+	slide.addEventListener('touchstart', () => {
+	    slide.style.height = '90%';
+	});
 
         slide.addEventListener('transitionend', (event) => {
             if (event.propertyName === 'height' && slide.style.height === '90%') {
@@ -68,6 +64,13 @@ $(document).ready(function(){
                 p.style.display = 'none';
             });
         });
+
+	slide.addEventListener('touchend', () => {
+	    slide.style.height = '60%';
+	    paragraphs.forEach(p => {
+	        p.style.display = 'none';
+	    });
+	});
         
        slide.addEventListener('click', () => {
             const url = slide.getAttribute('data-url');
